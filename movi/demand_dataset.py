d36be5b357ae190aacb02021dc3caf913babe5e8
Oleksandr Lysenko <sashkolysenko@gmail.com>
2020-03-28 20:35:31 +0100

    Add demand dataset module

---
+++ movi/demand_dataset.py
@@ -0,0 +1,62 @@
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+from torchvision import transforms
+
+from tools import points_per_cell
+
+
+class DemandDataset(Dataset):
+    """Dataset consists of rides "images" - to predict
+    next N minutes use aggregated demand for the past N minutes.
+
+    TODO: add static demand, e.g. average per hour per day per week
+
+    # From the paper (https://www.dropbox.com/s/ujqova12lnklgn5/dynamic-fleet-management-TR.pdf?dl=0)
+    # ..actual demand heat maps from the last two steps and constant
+    # planes with sine and cosine of day of week and hour of day
+    """
+
+    def __init__(self, rides, bounding_box, image_shape: Tuple[int, int]):
+        super().__init__()
+        # current demand
+        self.X = []
+        # future demand
+        self.y = []
+
+        # to predict demand for the next N minutes
+        # take N minutes of rides before
+        rides_before = None
+        for grp, next_rides in rides.groupby(rides.pickup_datetime):
+
+            if rides_before is not None:
+                x = points_per_cell(
+                    rides_before.pickup_lon,
+                    rides_before.pickup_lat,
+                    bounding_box,
+                    image_shape,
+                )
+
+                y = points_per_cell(
+                    next_rides.pickup_lon,
+                    next_rides.pickup_lat,
+                    bounding_box,
+                    image_shape,
+                )
+
+                self.X.append(x)
+                self.y.append(y)
+
+            rides_before = next_rides
+
+        print(f"Dataset size: {len(self.X)}")
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        x = self.X[idx]
+        y = self.y[idx]
+
+        transform = transforms.Compose([transforms.ToTensor()])
+        x = transform(x.astype(np.float32))
+        y = transform(y.astype(np.float32))
+        return x, y
