5644dcbd7c3dbefedbb13b24439379871a25af00
Oleksandr Lysenko <sashkolysenko@gmail.com>
2020-05-20 15:52:39 +0200

    Add file with tools

---
+++ demand-prediction/tools.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+
+def MAD_outliers(data: pd.Series) -> pd.Series:
+    """Detect outliers use MAD based method and return a binary index
+
+    Detect outliers we can use robust method based on median absolute deviation ( Page 19, Data Cleaning, 2019 ):
+
+    >> ... the median and the median absolute deviation (MAD) that can replace mean and standard deviation, respectively.
+
+    >> The median and MAD lead to a robust outlier detection technique known as Hampel X84 that is 
+    >> quite reliable in the face of outliers since it has a breakdown point of 50%. Hampel X84 marks 
+    >> outliers as those data points that are more than 1.4826θ MADs away from the median, where θ is 
+    >> the number of standard deviations away from the mean one would have used if there were no 
+    >> outliers in the dataset. The constant 1.4826 is derived under a normal distribution, where one 
+    >> standard deviation away from the mean is about 1.4826 MADs.
+    
+    """
+    num_std = 3
+    theta = 1.4826
+    
+    median = data.median()
+    mad = np.median((data - median).abs())
+    
+    lower = median - mad * (theta * num_std)
+    upper = median + mad * (theta * num_std)
+
+    return ((data < lower) | (data > upper))
